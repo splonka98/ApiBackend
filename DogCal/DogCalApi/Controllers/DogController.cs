f2082f50f359a5e518fbe0d95d53c6ca8b8e6e21
splonka98 <szymon.plonka@microsoft.wsei.edu.pl>
2023-09-14 15:24:07 +0200

    jwt trying

--- DogCal/DogCalApi/Controllers/DogController.cs
+++ DogCal/DogCalApi/Controllers/DogController.cs
@@ -4,6 +4,11 @@ using AppCore.Models;
 using Infractructure;
 using AppCore.Interfaces;
 using DogCalApi.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using System.Net.Http.Headers;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace DogCalApi.Controllers
 {
@@ -13,15 +18,34 @@ namespace DogCalApi.Controllers
     {
         private readonly ApiDbContext _context;
         private readonly IDogService _dogService;
-        public DogController(ApiDbContext context, IDogService dogService)
+        private string[] _claims;
+        HttpClient _client;
+        public DogController(ApiDbContext context, IDogService dogService, HttpClient client)
         {
             _context = context;
             _dogService = dogService;
+            _client = client;
         }
-
+        [Authorize]
+        [HttpGet]
+        public JsonResult SecuredEndpoint(string token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var readenToken = tokenHandler.ReadToken(token);
+            if (readenToken == null) return new JsonResult(NotFound());
+            
+            // Pobierz konkretny claim na podstawie jego typu (nazwy).
+            var nameIdentifierClaim = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role);
+            // Jeśli dotarliśmy do tego miejsca, to token jest poprawny.
+            // Kod, który dostarcza chronione dane.
+            return new JsonResult(Ok(token));
+        }
+        [Authorize]
         [HttpPost]
         public JsonResult CreateEdit(AddEditDogDto dogDto)
         {
+            var userClaims = User.Claims.ToList();
+            var userId= User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
             Dog dog = _dogService.AddOrEditDog(dogDto.Id, dogDto.Name, dogDto.Weight, dogDto.ActivityLevel, dogDto.OwnerId);
             _dogService.CalculateCalories(dog);
             if (dog.Id == 0)
@@ -43,7 +67,7 @@ namespace DogCalApi.Controllers
 
             return new JsonResult(Ok(dog));
         }
-
+        [Authorize]
         [HttpGet]
         public JsonResult Get(int id)
         {
@@ -66,7 +90,7 @@ namespace DogCalApi.Controllers
 
             return new JsonResult(NoContent());
         }
-
+        [Authorize(Roles = "1")]
         [HttpGet]
         public JsonResult GetAll()
         {
--- DogCal/DogCalApi/Program.cs
+++ DogCal/DogCalApi/Program.cs
@@ -7,6 +7,10 @@ using Microsoft.Extensions.Options;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 using Microsoft.AspNetCore.Hosting;
 using DogCalApi;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,6 +30,21 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IDogService, DogService>();
+builder.Services.AddScoped<HttpClient>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.RequireHttpsMetadata = false;
+            options.SaveToken = true;
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                ValidAudience = builder.Configuration["Jwt:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+            };
+        });
 
 var app = builder.Build();
 
@@ -35,10 +54,18 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
-
+app.UseRouting();
 app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+
+});
+
+
+//app.UseHttpsRedirection();
+
+
 
 app.MapControllers();
 
